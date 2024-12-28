1976c3f3f3f7efdbd7f886c387d0a4566e10498d
shradmecen <martyna.leszczynska00@gmail.com>
2024-12-28 14:42:38 +0100

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
         ExceptionGenerator generator = new ExceptionGenerator();
 
-        try {
-            generator.generateException();
-        } catch (NullPointerException e) {
-            System.out.println("Wystąpił wyjątek NullPointerException: " + e.toString());
-            e.printStackTrace();
+        String s = null;
+        try
+        {
+            if (s.equals("gfg"))
+                System.out.print("Same");
+            else
+                System.out.print("Not Same");
+        }
+        catch(NullPointerException e)
+        {
+            System.out.print("NullPointerException Caught");
         }
     }
 }
